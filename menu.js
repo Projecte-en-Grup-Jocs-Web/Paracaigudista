ef8619a003454bac1e1f9623b3d21effda8131d0
AlexCG-udg <u1995380@campus.udg.edu>
2025-06-02 18:27:33 +0200

    opcions css, opcions.js, opcions.html modificats

--- menu.js
+++ menu.js
@@ -3,3 +3,7 @@ document.getElementById('BotoJugar').addEventListener('click', function() {
         document.getElementById('game-container').style.display = 'block';
         startGame();
     });
+
+document.getElementById('BotoOpcions').addEventListener('click', function() {
+        window.location.href = 'opcions.html';
+    });
