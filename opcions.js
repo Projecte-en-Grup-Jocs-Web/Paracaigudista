b6e466944efa9d0a54fbdc30f2b3b33afd7a0634
AlexCG-udg <u1995380@campus.udg.edu>
2025-06-03 11:53:30 +0200

    Opcions funcionals amb local Storage, falta modificar el game.js

--- opcions.js
+++ opcions.js
@@ -1,44 +1,29 @@
-document.getElementById('tornar').addEventListener('click', function () {
-  window.location.href = 'menu.html';
-});
+$(document).ready(function () {
+  const defaultOpcions = {
+    difficulty: 'normal'
+  };
 
+  const $difficulty = $('#difficulty');
 
-var options = function(){
-    const default_opcions = {
-        difficulty:'normal',
+  let opcions = JSON.parse(localStorage.getItem('opcions') || JSON.stringify(defaultOpcions));
 
-    };
+  $difficulty.val(opcions.difficulty);
 
-    var difficulty = $('#difficulty');
+  $difficulty.on('change', function () {
+    opcions.difficulty = $difficulty.val();
+  });
 
-    var opcions = JSON.parse(localStorage.opcions||JSON.stringify(default_opcions));
-        
-    difficulty.on('change',()=>opcions.difficulty = difficulty.val());
+  $('#apply').on('click', function () {
+    localStorage.setItem('opcions', JSON.stringify(opcions));
+    location.href = "menu.html";
+  });
 
-    difficulty.val(opcions.difficulty);
+  $('#default').on('click', function () {
+    opcions.difficulty = defaultOpcions.difficulty;
+    $difficulty.val(opcions.difficulty);
+  });
 
-    return { 
-        applyChanges: function(){
-            localStorage.opcions = JSON.stringify(opcions);
-        },
-        defaultValues: function(){
-            opcions.difficulty = default_opcions.difficulty;
-            difficulty.val(opcions.difficulty);
-           
-        }
-    }
-}();
-
-
-document.getElementById('apply').addEventListener('click', function () {
-    options.applyChanges();
-    location.assign("../");
-});
-
-document.getElementById('default').addEventListener('click', function () {
-    options.defaultValues();
-});
-
-document.getElementById('tornar').addEventListener('click', function () {
-  window.location.href = 'menu.html';
+  $('#tornar').on('click', function () {
+    window.location.href = 'menu.html';
+  });
 });
