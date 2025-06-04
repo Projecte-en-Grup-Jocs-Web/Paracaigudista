b58c5c243c40bcb86dd907c9ae419e480bf2b9bc
Martí <martijordianna@gmail.com>
2025-06-04 18:30:30 +0200

    revisió comentaris menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,13 @@
+
+// Amaga menu mostra partida 
 $('#BotoJugar').on('click', function () {
   $('#menu').hide();
   $('#game-container').show();
   startGame();
 });
 
+
+//redireccio a opcions
 $('#BotoOpcions').on('click', function () {
   window.location.href = 'html/opcions.html';
 });
