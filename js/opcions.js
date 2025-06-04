0de9caeff5620443957a3a3ed607ab6b2bef24cb
Martí <martijordianna@gmail.com>
2025-06-04 18:29:09 +0200

    revisió comentaris opcions.js

--- js/opcions.js
+++ js/opcions.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //dificultat estandar si localStorage vuit
   const defaultOpcions = {
     difficulty: 'normal'
   };
@@ -8,21 +9,21 @@ $(document).ready(function () {
   let opcions = JSON.parse(localStorage.getItem('opcions') || JSON.stringify(defaultOpcions));
 
   $difficulty.val(opcions.difficulty);
-
+  //canviem variable temporal
   $difficulty.on('change', function () {
     opcions.difficulty = $difficulty.val();
   });
-
+  //actualitzem loaclStorage
   $('#apply').on('click', function () {
     localStorage.setItem('opcions', JSON.stringify(opcions));
     location.href = "../index.html";
   });
-
+  //actualitzem  variable temporal
   $('#default').on('click', function () {
     opcions.difficulty = defaultOpcions.difficulty;
     $difficulty.val(opcions.difficulty);
   });
-
+  //sortir opcions
   $('#tornar').on('click', function () {
     window.location.href = '../index.html';
   });
